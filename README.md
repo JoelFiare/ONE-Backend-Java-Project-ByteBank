3103de005199beb4e6a89d9eff9c475a7a5df3a1
JoelFiare <JoelFiare@gmail.com>
2023-06-11 19:56:49 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ONE Oracle Next Education - BackEnd Java Proyecto ByteBank - Alura G5
+
+Ejercicios del curso.
---
+++ src/project/bytebank/Cuenta.java
@@ -0,0 +1,7 @@
+package project.bytebank;
+
+public class Cuenta {
+    public static void main(String[] args) {
+        System.out.println("holamundo");
+    }
+}
